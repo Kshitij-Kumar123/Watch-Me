e3f6913396b12828a2c4a9635c413f865d0cf82b
Kshitij-Kumar123 <68606208+Kshitij-Kumar123@users.noreply.github.com>
2023-04-09 10:43:33 -0400

    add authorizer + fix cors with auth

--- aws-python-scheduled-cron-project/handler.py
+++ aws-python-scheduled-cron-project/handler.py
@@ -4,6 +4,7 @@ import logging
 import boto3
 import random
 import os
+import re
 import uuid
 from botocore.exceptions import ClientError
 
@@ -21,8 +22,12 @@ def build_resp(body, status_code=200, content_type="application/json"):
     return {
         'statusCode': status_code,
         'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            # 'Content-Type': 'application/json',
+            # 'Access-Control-Allow-Origin': '*',
+            # 'Access-Control-Allow-Methods': '*',
+            # 'Access-Control-Allow-Credentials': '*'
         },
         'body': json.dumps(body),
         "isBase64Encoded": False
@@ -41,11 +46,40 @@ def pre_sign_up(event, context):
     response = table.put_item(
         Item={
             'userId': new_user_id,
-            "subscribeStatus": False
+            "subscribeStatus": False,
+            'incident': {
+                "allow": {
+                    "/incident/86f9d64d-2099-4664-8c7d-c759c667a844": "GET"
+                }
+            }
         }
     )
 
     # TODO: Send email for confirmation and greeting
+
+    return event
+
+
+def update_acl(event, context):
+    # Read dynamoDB stream from paystub table
+    for record in event['Records']:
+        principalId = record['dynamodb']['Keys']['userId']['S']
+
+        client = boto3.client('dynamodb')
+        response = record['dynamodb']['NewImage']
+        table = boto3.resource('dynamodb').Table(os.environ.get('tableName'))
+        response = table.update_item(
+            # Update acl table
+            Key={
+                'userId': principalId
+            },
+            UpdateExpression="set sendEmail.allow=:a",
+            ExpressionAttributeValues={
+                ":a": {
+                    "/sendEmail": "POST"
+                }
+            },
+        )
 
     return event
 
@@ -198,11 +232,11 @@ def send_email(event, context):
 
 
 # TODO
-# CRUD with incidents -- done
+# CRUD with incidents -- v1 done
 # Add authorization and endpoint restriction for such incidents --
 # Send emails to users to info they are subscribed to: insight emails about ticket with quote
+# ask gpt?? quick feature??
 
-# TODO: Incidents CRUD
 def get_incidents(event, context):
     # Get all subscribers of the incident by pkey (id) and/or skey (active)
     incidents_table = str(os.environ['INCIDENTS_TABLE'])
@@ -320,3 +354,165 @@ def get_subscribers(event, context):
     post = "get subscribers"
 
     return build_resp(body={"post": post})
+
+
+# Authorization handlers
+
+def authorization(event, context):
+
+    print(event)
+    token = event['authorizationToken']
+    client = boto3.client('cognito-idp')
+    response = client.get_user(AccessToken=token)
+
+    # get user uuid
+    principalId = response['UserAttributes'][0]['Value']
+
+    awsAccountId = event['methodArn'].split(':')[4]
+    # Configure your policy: restApiId, region, stage, etc.
+    policy = AuthPolicy(principalId, awsAccountId)
+
+    # Get rules from auth table
+    client = boto3.client('dynamodb')
+    response = client.get_item(
+        TableName=os.environ.get('tableName'),
+        Key={'userId': {"S": principalId}})
+    # Add your rules to policy here
+    # example:
+    for k, v in response['Item'].items():
+        # policy.allowMethod(v['S'], k)
+        if k != 'userId':
+            for k, v in v['M']['allow']['M'].items():
+                policy.allowMethod(v['S'], k)
+
+    # Build policy
+    authResponse = policy.build()
+    return authResponse
+
+
+class HttpVerb:
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    PATCH = "PATCH"
+    HEAD = "HEAD"
+    DELETE = "DELETE"
+    OPTIONS = "OPTIONS"
+    ALL = "*"
+
+
+class AuthPolicy(object):
+    awsAccountId = ""
+    principalId = ""
+    version = "2012-10-17"
+    pathRegex = "^[/.a-zA-Z0-9-\*]+$"
+    allowMethods = []
+    denyMethods = []
+
+    restApiId = "*"
+    region = "*"
+    stage = "*"
+
+    def __init__(self, principal, awsAccountId):
+        self.awsAccountId = awsAccountId
+        self.principalId = principal
+        self.allowMethods = []
+        self.denyMethods = []
+
+    def _addMethod(self, effect, verb, resource, conditions):
+        if verb != "*" and not hasattr(HttpVerb, verb):
+            raise NameError("Invalid HTTP verb " + verb +
+                            ". Allowed verbs in HttpVerb class")
+        resourcePattern = re.compile(self.pathRegex)
+        if not resourcePattern.match(resource):
+            raise NameError("Invalid resource path: " + resource +
+                            ". Path should match " + self.pathRegex)
+
+        if resource[:1] == "/":
+            resource = resource[1:]
+
+        resourceArn = ("arn:aws:execute-api:" +
+                       self.region + ":" +
+                       self.awsAccountId + ":" +
+                       self.restApiId + "/" +
+                       self.stage + "/" +
+                       verb + "/" +
+                       resource)
+
+        if effect.lower() == "allow":
+            self.allowMethods.append({
+                'resourceArn': resourceArn,
+                'conditions': conditions
+            })
+        elif effect.lower() == "deny":
+            self.denyMethods.append({
+                'resourceArn': resourceArn,
+                'conditions': conditions
+            })
+
+    def _getEmptyStatement(self, effect):
+        statement = {
+            'Action': 'execute-api:Invoke',
+            'Effect': effect[:1].upper() + effect[1:].lower(),
+            'Resource': []
+        }
+
+        return statement
+
+    def _getStatementForEffect(self, effect, methods):
+        statements = []
+
+        if len(methods) > 0:
+            statement = self._getEmptyStatement(effect)
+
+            for curMethod in methods:
+                if curMethod['conditions'] is None or len(curMethod['conditions']) == 0:
+                    statement['Resource'].append(curMethod['resourceArn'])
+                else:
+                    conditionalStatement = self._getEmptyStatement(effect)
+                    conditionalStatement['Resource'].append(
+                        curMethod['resourceArn'])
+                    conditionalStatement['Condition'] = curMethod['conditions']
+                    statements.append(conditionalStatement)
+
+            statements.append(statement)
+
+        return statements
+
+    def allowAllMethods(self):
+        self._addMethod("Allow", HttpVerb.ALL, "*", [])
+
+    def denyAllMethods(self):
+        self._addMethod("Deny", HttpVerb.ALL, "*", [])
+
+    def allowMethod(self, verb, resource):
+        self._addMethod("Allow", verb, resource, [])
+
+    def denyMethod(self, verb, resource):
+        self._addMethod("Deny", verb, resource, [])
+
+    def allowMethodWithConditions(self, verb, resource, conditions):
+        self._addMethod("Allow", verb, resource, conditions)
+
+    def denyMethodWithConditions(self, verb, resource, conditions):
+        self._addMethod("Deny", verb, resource, conditions)
+
+    def build(self):
+        if ((self.allowMethods is None or len(self.allowMethods) == 0) and
+                (self.denyMethods is None or len(self.denyMethods) == 0)):
+            raise NameError("No statements defined for the policy")
+
+        policy = {
+            'principalId': self.principalId,
+            'policyDocument': {
+                'Version': self.version,
+                'Statement': []
+            }
+        }
+
+        policy['policyDocument']['Statement'].extend(
+            self._getStatementForEffect("Allow", self.allowMethods))
+        policy['policyDocument']['Statement'].extend(
+            self._getStatementForEffect("Deny", self.denyMethods))
+
+        return policy
