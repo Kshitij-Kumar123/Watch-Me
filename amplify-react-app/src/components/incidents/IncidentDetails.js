f20894d1aab7307307bba60678c4caa8cdfd8571
Kshitij-Kumar123 <68606208+Kshitij-Kumar123@users.noreply.github.com>
2023-04-21 19:50:27 -0400

    basic incident detail ui mockup

--- amplify-react-app/src/components/incidents/IncidentDetails.js
+++ amplify-react-app/src/components/incidents/IncidentDetails.js
@@ -10,8 +10,13 @@ import {
   Input,
   InputNumber,
   Select,
-  Statistic,
+  DatePicker,
+  List,
+  Tooltip,
 } from "antd";
+import { Comment } from "@ant-design/compatible";
+
+const { RangePicker } = DatePicker;
 
 export default function IncidentDetails() {
   const params = useParams();
@@ -23,7 +28,40 @@ export default function IncidentDetails() {
       setIncidentData(response.data);
     });
   }, []);
-
+  const data = [
+    {
+      actions: [<span key="comment-list-reply-to-0">Reply to</span>],
+      author: "Han Solo",
+      content: (
+        <p>
+          We supply a series of design principles, practical patterns and high
+          quality design resources (Sketch and Axure), to help people create
+          their product prototypes beautifully and efficiently.
+        </p>
+      ),
+      datetime: (
+        <Tooltip title="2016-11-22 11:22:33">
+          <span>8 hours ago</span>
+        </Tooltip>
+      ),
+    },
+    {
+      actions: [<span key="comment-list-reply-to-0">Reply to</span>],
+      author: "Han Solo",
+      content: (
+        <p>
+          We supply a series of design principles, practical patterns and high
+          quality design resources (Sketch and Axure), to help people create
+          their product prototypes beautifully and efficiently.
+        </p>
+      ),
+      datetime: (
+        <Tooltip title="2016-11-22 10:22:33">
+          <span>9 hours ago</span>
+        </Tooltip>
+      ),
+    },
+  ];
   return (
     <div style={{ padding: 16 }}>
       <Row gutter={[16, 16]}>
@@ -33,7 +71,7 @@ export default function IncidentDetails() {
             bordered={false}
           >
             <Row>
-              <Col span={12}>
+              <Col sm={24} md={12}>
                 <Form
                   labelCol={{ span: 12 }}
                   wrapperCol={{ span: 20 }}
@@ -46,33 +84,78 @@ export default function IncidentDetails() {
                   <Form.Item label="Incident Title" name="title">
                     <Input />
                   </Form.Item>
+                  <Form.Item label="Status" name="status">
+                    <Select>
+                      <Select.Option value="status">Status</Select.Option>
+                    </Select>
+                  </Form.Item>
+                  <Form.Item label="Description" name="description">
+                    <TextArea rows={5} />
+                  </Form.Item>
+                </Form>
+              </Col>
+              <Col sm={24} md={12}>
+                <Form
+                  labelCol={{ span: 12 }}
+                  wrapperCol={{ span: 20 }}
+                  layout="vertical"
+                  style={{ maxWidth: 600, padding: "16px 0px" }}
+                >
                   <Form.Item label="Sub Category" name="subCategory">
                     <Select>
                       <Select.Option value="demo">Demo</Select.Option>
                     </Select>
+                  </Form.Item>
+                  <Form.Item label="Developer" name="developer">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item label="Reporter" name="reporter">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item label="RangePicker" name="dateRange">
+                    <RangePicker />
                   </Form.Item>
                   <Form.Item label="Complexity" name="complexity">
                     <InputNumber />
                   </Form.Item>
                 </Form>
               </Col>
-              <Col span={12}>
-                <Form
-                  labelCol={{ span: 12 }}
-                    wrapperCol={{ span: 20 }}
-                  layout="vertical"
-                  style={{ maxWidth: 600, padding: "16px 0px" }}
-                >
-                  <Form.Item label="Description" name="description">
-                    <TextArea rows={4} />
-                  </Form.Item>
-                  <Form.Item label="Comments" name="comment">
-                    <TextArea rows={4} />
-                  </Form.Item>
-                </Form>
-              </Col>
             </Row>
           </Card>
+        </Col>
+        <Col>
+          <List
+            className="comment-list"
+            header={`${data.length} Comments`}
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  actions={item.actions}
+                  author={item.author}
+                  avatar={item.avatar ?? ""}
+                  content={item.content}
+                  datetime={item.datetime}
+                  style={{ padding: "0px 16px", margin: "16px 0px" }}
+                />
+              </li>
+            )}
+          />
+          <Form
+            labelCol={{ span: 12 }}
+            layout="vertical"
+            style={{ padding: "16px 0px" }}
+          >
+            <Form.Item label="Add Comment" name="Comment">
+              <TextArea rows={4} />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" style={{ float: "right" }}>
+                Add comment
+              </Button>
+            </Form.Item>
+          </Form>
         </Col>
       </Row>
     </div>
